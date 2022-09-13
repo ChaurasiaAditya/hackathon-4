a6eec285a2d82bdabf87361e3b49a31ec2479a2f
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 12:19:37 +0530

    Create a Read Ticket Data and Bust Ticket service Object in HelloTest class

--- src/test/java/com/jap/ticketing/HelloTest.java
+++ src/test/java/com/jap/ticketing/HelloTest.java
@@ -1,7 +1,15 @@
 package com.jap.ticketing;
 
-import static org.junit.Assert.*;
+import com.jap.ticketing.service.BusTicketService;
+import com.jap.ticketing.service.ReadTicketData;
+import org.junit.jupiter.api.BeforeEach;
+
+
 
 public class HelloTest {
+    ReadTicketData readTicketData;
+    BusTicketService busTicketService;
+
+
 
 }
