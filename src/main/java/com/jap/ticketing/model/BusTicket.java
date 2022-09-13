88fd7be418daa72fc36931b31b32ae90f44abfd8
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 10:24:05 +0530

    created required private attributes in BusTicket class

--- src/main/java/com/jap/ticketing/model/BusTicket.java
+++ src/main/java/com/jap/ticketing/model/BusTicket.java
@@ -6,4 +6,14 @@
 package com.jap.ticketing.model;
 
 public class BusTicket {
+    private String scheduleNumber;
+    private String routeNumber;
+    private int ticketFromStopId;
+    private int ticketFromStopSeqNumber;
+    private int ticketTillStopId;
+    private int ticketTillStopSeqNumber;
+    private String ticketDate;
+    private String ticketTime;
+    private double totalTicketAMount;
+    private double travelledKM;
 }
