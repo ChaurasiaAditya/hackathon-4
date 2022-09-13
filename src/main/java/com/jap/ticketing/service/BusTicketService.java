8cbf355762155c9814f9b2ccaa88958f1b6bebcf
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 11:06:44 +0530

    written the logic to short bus ticket by distance travelled using anonymous class in getAllTicketDetailSortedByDistanceTravelled method in BusTicketService class

--- src/main/java/com/jap/ticketing/service/BusTicketService.java
+++ src/main/java/com/jap/ticketing/service/BusTicketService.java
@@ -7,11 +7,19 @@ package com.jap.ticketing.service;
 
 import com.jap.ticketing.model.BusTicket;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class BusTicketService {
 
     public List<BusTicket> getAllTicketDetailSortedByDistanceTravelled(List<BusTicket> busTicketList){
-        return null;
+
+        Comparator<BusTicket> BusTicketComparator = new Comparator<BusTicket>() {
+            @Override
+            public int compare(BusTicket o1, BusTicket o2) {
+                return Double.compare(o2.getTravelledKM(),o1.getTravelledKM());
+            }
+        };busTicketList.sort(BusTicketComparator);
+        return busTicketList;
     }
 }
