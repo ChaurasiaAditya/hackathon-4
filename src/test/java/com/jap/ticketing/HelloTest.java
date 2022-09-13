4078e6e0fb4defb8de5bcf0d6e25f12505f440f5
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 12:33:47 +0530

    Added @Test annotation in Method

--- src/test/java/com/jap/ticketing/HelloTest.java
+++ src/test/java/com/jap/ticketing/HelloTest.java
@@ -1,10 +1,14 @@
 package com.jap.ticketing;
 
+import com.jap.ticketing.model.BusTicket;
 import com.jap.ticketing.service.BusTicketService;
 import com.jap.ticketing.service.ReadTicketData;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
 
 
 public class HelloTest {
@@ -22,5 +26,11 @@ public class HelloTest {
     public void tearDown(){
         readTicketData = null;
         busTicketService = null;
+    }
+
+    @Test
+    public void givenBusTicketDataFileReturnTheNUmberOfListPresentInFile(){
+    List<BusTicket> output = readTicketData.busTicketList(fileName);
+    Assertions.assertEquals(49,output.size());
     }
 }
