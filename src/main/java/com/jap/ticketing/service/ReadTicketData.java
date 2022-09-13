5c78a040c69bcebf44c899673d27bc190e245975
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 10:30:11 +0530

    created ReadTicketData class in service package

---
+++ src/main/java/com/jap/ticketing/service/ReadTicketData.java
@@ -0,0 +1,9 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 13-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.jap.ticketing.service;
+
+public class ReadTicketData {
+}
