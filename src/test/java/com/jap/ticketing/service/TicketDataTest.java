9f6802a004356a3b99ec769b95bbd250227ddf67
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 17:06:13 +0530

    Added TicketDataTest

--- src/test/java/com/jap/ticketing/service/TicketDataTest.java
+++ src/test/java/com/jap/ticketing/service/TicketDataTest.java
@@ -1,10 +1,12 @@
 package com.jap.ticketing.service;
 
+import com.jap.ticketing.model.BusTicket;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class TicketDataTest {
     TicketData ticketData;
@@ -21,6 +23,13 @@ class TicketDataTest {
     }
 
     @Test
-    void busTicketList() {
+    public void givenBusTicketDataFileReturnTheNUmberOfListPresentInFile() {
+        List<BusTicket> busTicketList = ticketData.busTicketList(fileName);
+        Assertions.assertEquals(49, busTicketList.size(), "Error in reading file correctly");
+    }
+    @Test
+    public void givenBusTicketDataFileReturnTheNUmberOfListPresentInFileFailure() {
+        List<BusTicket> busTicketList = ticketData.busTicketList(fileName);
+        Assertions.assertNotEquals(50, busTicketList.size(), "Error in reading file correctly");
     }
 }
