9e636a2d579e32fa27570976814da1fd68c8e607
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-13 11:46:47 +0530

    Called the read ticket method and stored in an Array List

--- src/main/java/com/jap/ticketing/Hello.java
+++ src/main/java/com/jap/ticketing/Hello.java
@@ -1,11 +1,16 @@
 package com.jap.ticketing;
-
-import com.jap.ticketing.service.BusTicketService;
+import com.jap.ticketing.model.BusTicket;
 import com.jap.ticketing.service.ReadTicketData;
+import java.util.List;
 
 public class Hello {
 
     public static void main(String[] args) {
+
+        // create an object for Read ticket class
         ReadTicketData readTicketData = new ReadTicketData();
+
+        // Call the read ticket method and store in an Array List
+        List<BusTicket> busTicketList = readTicketData.busTicketList("src/main/resources/sample.csv");
     }
 }
